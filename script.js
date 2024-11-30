005b91012d4145dbd91b25ef1e5ad542197dae02
tomos4ki <sarimu.medfai@gmail.com>
2024-11-30 12:13:35 +0100

    error fix

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // script fo scheck if there is a cookie in the system for this site
 
 window.onload = function() {
+    console.log('Windoow loaded');
     //checking if the cookie exisits
     const cookie = getCookie('websiteConfig');
     if(!cookie){
         //displaying the folder selector to map it
 
         document.getElementById('folder-selector').style.display = 'block';
+        console.log('Folder selector: ')
 
     }
     else{
@@ -20,12 +22,38 @@ window.onload = function() {
 
 document.getElementById('link-folder-btn').addEventListener('click', function() {
     //getting the selected folder
-
+function searchJsonFile(){
     const folder = document.getElementById('forlder-input').files[0].path;
+    console.lo('folder is : ', folder);
+    const jsonFilePath = `${folder}/config/config.json`;
+    
+    
+    
+    //check and create other required folders and files
+    
+    if (!fs.existsSync(`${folder}/config/thumbnails`)) {
+        //creating the thumbnail folder
+        fs.mkdirCync(`${folder}/config/thumbnails`);
+    }
+    
     
     //checking if the required json file exsists
-    //-------------------see you stopped here tomo-----------
-    //------see the text file from where you stopped to continue----
+
+    if(fs.existsSync(jsonFilePath)){
+        // the JSON file exists, returning th epath to the other functions to use
+        return jsonFilePath;
+    }
+    else{
+        //the json file don't exists, creating it
+        const jsonData = {
+            //default config data
+        };
+        fs.writeFileSync(`${folder}/config/config.json`, JSON.stringify(jsonData));
+        return jsonFilePath;
+    }
+
+    
+}
     
     
     
@@ -35,9 +63,16 @@ document.getElementById('link-folder-btn').addEventListener('click', function() 
     setCookie('websiteConfig', folder, 30);//expires in 30 days
     //hiding the folder selector
     document.getElementById('folder-selector').style.display = 'none';
-    // configure the website with the folder imputs
+    // configure the website with the folder information
     configurewebsite(folder)
 });
+
+
+// TO DO : immmplement the logic to configure the website using the json file path
+//...
+
+
+
 
 //helper function to get the cookie
 function getCookie(name) {
@@ -46,7 +81,7 @@ function getCookie(name) {
     if (parts.lenght === 2) return parts.pop().split(';').shift();
 }
 
-//helper function to get the cookie
+//helper function to set a cookie
 
 function setCookie(name, value, days){
     const date = new Date();
