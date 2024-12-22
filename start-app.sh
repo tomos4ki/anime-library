cb140191299adc891e5f65513d8622e4c02e11fe
tomos4ki <sarimu.medfai@gmail.com>
2024-12-22 20:23:17 +0100

    updating shell (still not 100 sure it's working)

--- start-app.sh
+++ start-app.sh
@@ -3,29 +3,42 @@
 # detect the operating system
 
 OS=$(uname -s)
+problem = "if you have a problem please go to https://github.com/tomos4ki/anime-library/issues and make new issue regaring your problem."
+
 
 #check if httpserver is installed
 
-if ! command -v http-server &> /dev/null
+if command -v http-server &> /dev/null
 then
-    #install http-server based on the operating system
-    if [ "$OS" == "linux" ]; then
-        #Installing http-server on linux (kali what i have currently)
-        echo "installing http-server on linux ..."
-        npm install -g http-server
-    elif [ "$OS" == "MINGW"* ]; then
-        #Installing http-server on windows(using GIT Bash)
-    elif [ "$OS" == "Darwin" ]; then
-        #Installing http-server on macos
-        echo "installing http-server on macos ..."
-        npm install -g http-server
-    elif [ "$OS" == "Windows" ]; then
-        #Installing http-server on windows
-        echo "installing http-server on windows ..."
+    echo "http-server is already installed. Skipping installation..."
+else
+    #checking if npm is installed
+    if command -v npm &> /dev/null
+    then
+        echo "npm is already installed. Installing http-server using npm..."
         npm install -g http-server
     else
-        echo "Operating system not supported"
-        exit 1
+        #install npm and http-server based on the operating system
+        if [ "$OS" == "linux" ]; then
+            #Installing http-server on linux (kali what i have currently)
+            echo "installing http-server on linux ..."
+            sudo apt-get update
+            sudo apt-get install -y npm
+            sudo npm install -g http-server
+        elif [ "$OS" == "Darwin" ]; then
+            #Installing http-server on macos
+            echo "installing http-server on macos ..."
+            brew install npm
+            npm install -g http-server
+        elif [ "$OS" == "WINGW32_NT" ]; then
+            #Installing http-server on windows
+            echo "installing http-server on windows ..."
+            npm install -g npm
+            npm install -g http-server
+        else
+            echo "Operating system not supported: $(uname -s)"
+            exit 1
+        fi
     fi
 fi
 
@@ -37,4 +50,13 @@ http-server p 8080 a localhost .
 sleep 1
 
 #open the server in the default web browser
-xdg-open http://localhost:8080
+if [ "$OS" == "linux" ]; then
+    xdg-open http://localhost:8080
+elif [ "$OS" == "Darwin" ]; then
+    open http://localhost:8080
+elif [ "$OS" == "WINGW32_NT" ]; then
+    start http://localhost:8080
+else
+    echo "Operating system not supported: $(uname -s)"
+    exit 1
+fi
