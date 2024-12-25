80290842c30ca9b0272975ed7c197ff5db6a7923
tomos4ki <sarimu.medfai@gmail.com>
2024-12-25 16:12:11 +0100

    update

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ def check_config():
             "exists" : False
         })
 
-@app.route('/create_config', methodS=['POST'])
+@app.route('/create_config', methods=['POST'])
 def create_config():
     anime_folder = request.json['anime_folder']
     config = {
@@ -37,20 +37,51 @@ def create_config():
         "anime_folder" : anime_folder
         })
 
-@app.route('/check_config', methods=['GET'])
+@app.route("/upload_config", methods=['POST'])
+def upload_config():
+    try:
+        config = request.json
+        if 'anime_folder' not in config:
+            return jsonify({
+                "success" : False,
+                "message" : "invalid config file : animefolder is missing"
+            })
+        with open(CONFIG_FILE, 'w') as f:
+            json.dump(config, f)
+        return jsonify({
+            "success" : True,
+            "message" : "config uploaded successfully",
+            "anime_folder" : config['anime_folder']
+        })
+    except Exception as e:
+        return jsonify({
+            "success" : False,
+            "message" : f"Error uploading config: {str(e)}"
+        })
+
+@app.route('/list_files', methods=['GET'])
 def list_files():
     directory = request.json['directory']
-    files = os.listdir(directory)
-    return jsonify(files)
+    try:
+        files = os.listdir(directory)
+        return jsonify({
+            "success" : True,
+            "files" : files
+        })
+    except Exception as e:
+        return jsonify({
+            "success" : False,
+            "message" : f"Error listing files :{str(e)}"
+        })
 
-@app.route('/create_folder', method=["POST"])
+@app.route('/create_folder', methods=["POST"])
 def create__folder():
     folder_path = request.json['folder_path']
     try:
         os.makedirs(folder_path, exist_ok=True)
         return jsonify({"success": True, "message": "folder cerated successfully"})
     except Exception as e:
-        return jsonify({"success": False, "message": str(e)})
+        return jsonify({"success": False, "message": f"Error creating folder :{str(e)}"})
     
 if __name__ == "__main___":
     app.run(port=5000)
