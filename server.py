900b58ec7f98854aaad79c8595f008d1d68ec049
tomos4ki <sarimu.medfai@gmail.com>
2024-12-25 12:14:44 +0100

    update

--- server.py
+++ server.py
@@ -7,7 +7,23 @@ app = Flask(__name__)
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG_FILE = os.path.join(APP_DIR, 'config.json')
 
-@app.route('/list_files', methods=['POST'])
+
+@app.route('/check_config', methods=['GET'])
+def check_config():
+    if os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, 'r') as f :
+            config = json.load(f)
+        return jsonify({
+            "exists" : True,
+            "anime_folder" : config.get("animefolder"),
+            "message" : "config file found."
+            })
+    else:
+        return jsonify({
+            "exists" : False
+        })
+
+@app.route('/check_config', methods=['GET'])
 def list_files():
     directory = request.json['directory']
     files = os.listdir(directory)
